04588d035d36748d9ae081bbc1660e8686ec17ce
vaibhav2.gupta <vaibhav2.gupta@paytmbank.com>
2023-01-04 01:29:47 +0530

    Adding and removing fragments

--- app/src/main/java/com/example/fragments_android/MainActivity.kt
+++ app/src/main/java/com/example/fragments_android/MainActivity.kt
@@ -3,19 +3,26 @@ package com.example.fragments_android
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.fragment.app.FragmentManager
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
-    lateinit var fragmentOne: FragmentOne
+    private var fragmentOne: FragmentOne? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        if (savedInstanceState == null) {
+        if (savedInstanceState != null) {
+            val fragment = supportFragmentManager.findFragmentById(R.id.root_frame)
+            if (fragment != null && fragment is FragmentOne) {
+                fragmentOne = fragment
+            }
+        }
+        btnAdd.setOnClickListener {
             fragmentOne = FragmentOne()
-            supportFragmentManager.beginTransaction()
-                .add(R.id.root_frame, fragmentOne)
-                .commit()
-        } else {
-            fragmentOne = supportFragmentManager.findFragmentById(R.id.root_frame) as FragmentOne
+            supportFragmentManager.beginTransaction().add(R.id.root_frame, fragmentOne!!).commit()
+        }
+        btnRemove.setOnClickListener {
+            if (fragmentOne != null)
+                supportFragmentManager.beginTransaction().remove(fragmentOne!!).commit()
         }
     }
 }
