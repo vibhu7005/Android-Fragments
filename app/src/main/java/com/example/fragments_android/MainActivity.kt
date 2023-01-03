37c7de79045ae11ba9693f75d7a2dc06576e0408
vaibhav2.gupta <vaibhav2.gupta@paytmbank.com>
2023-01-04 00:40:54 +0530

    Adding fragment dynamically.

--- app/src/main/java/com/example/fragments_android/MainActivity.kt
+++ app/src/main/java/com/example/fragments_android/MainActivity.kt
@@ -2,10 +2,20 @@ package com.example.fragments_android
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.FragmentManager
 
 class MainActivity : AppCompatActivity() {
+    lateinit var fragmentOne: FragmentOne
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        if (savedInstanceState == null) {
+            fragmentOne = FragmentOne()
+            supportFragmentManager.beginTransaction()
+                .add(R.id.root_frame, fragmentOne)
+                .commit()
+        } else {
+            fragmentOne = supportFragmentManager.findFragmentById(R.id.root_frame) as FragmentOne
+        }
     }
 }
