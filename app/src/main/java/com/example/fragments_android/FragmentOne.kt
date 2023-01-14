c617f6bbad1a0096d9abec57168942db5f4f8151
vaibhav2.gupta <vaibhav2.gupta@paytmbank.com>
2023-01-14 23:51:30 +0530

    Restored fragment code.

--- app/src/main/java/com/example/fragments_android/FragmentOne.kt
+++ app/src/main/java/com/example/fragments_android/FragmentOne.kt
@@ -1,18 +1,75 @@
 package com.example.fragments_android
 
+import android.content.Context
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 
 class FragmentOne : Fragment() {
-
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
+        Log.d("vaibhav", "F onCreateView")
         return inflater.inflate(R.layout.fragment_one, container, false)
     }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        Log.d("vaibhav", "F onCreate")
+        super.onCreate(savedInstanceState)
+    }
+
+    override fun onAttach(context: Context) {
+        Log.d("vaibhav", "F onAttach")
+        super.onAttach(context)
+    }
+
+    override fun onViewStateRestored(savedInstanceState: Bundle?) {
+        super.onViewStateRestored(savedInstanceState)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        Log.d("vaibhav", "F onViewCreated")
+        super.onViewCreated(view, savedInstanceState)
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        Log.d("vaibhav", "F onActivityCreated")
+        super.onActivityCreated(savedInstanceState)
+    }
+
+    override fun onStart() {
+        Log.d("vaibhav", "F onStart")
+        super.onStart()
+    }
+
+    override fun onResume() {
+        Log.d("vaibhav", "F onResume")
+        super.onResume()
+    }
+
+    override fun onPause() {
+        Log.d("vaibhav", "F onPause")
+        super.onPause()
+    }
+
+    override fun onDestroy() {
+        Log.d("vaibhav", "F onDestroy")
+        super.onDestroy()
+    }
+
+    override fun onDestroyView() {
+        Log.d("vaibhav", "F onDestroyView")
+        super.onDestroyView()
+    }
+
+    override fun onDetach() {
+        Log.d("vaibhav", "F onDetach")
+        super.onDetach()
+    }
+
 }
--- app/src/main/java/com/example/fragments_android/MainActivity.kt
+++ app/src/main/java/com/example/fragments_android/MainActivity.kt
@@ -2,16 +2,17 @@ package com.example.fragments_android
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import androidx.fragment.app.FragmentManager
+import android.util.Log
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     private var fragmentOne: FragmentOne? = null
     override fun onCreate(savedInstanceState: Bundle?) {
+        Log.d("vaibhav", "onCreate")
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         if (savedInstanceState != null) {
-            val fragment = supportFragmentManager.findFragmentById(R.id.root_frame)
+            val fragment = supportFragmentManager.findFragmentById(R.id.fragment_container)
             if (fragment != null && fragment is FragmentOne) {
                 fragmentOne = fragment
             }
@@ -25,4 +26,35 @@ class MainActivity : AppCompatActivity() {
                 supportFragmentManager.beginTransaction().remove(fragmentOne!!).commit()
         }
     }
+
+    override fun onStart() {
+        Log.d("vaibhav", "onStart")
+        super.onStart()
+    }
+
+    override fun onResume() {
+        Log.d("vaibhav", "onResume")
+        super.onResume()
+    }
+
+    override fun onPause() {
+        Log.d("vaibhav", "onPause")
+        super.onPause()
+    }
+
+    override fun onStop() {
+        Log.d("vaibhav", "onStop")
+        super.onStop()
+    }
+
+    override fun onDestroy() {
+        Log.d("vaibhav", "onDestroy")
+        super.onDestroy()
+    }
+
+    override fun onRestart() {
+        Log.d("vaibhav", "onRestart")
+        super.onRestart()
+    }
+
 }
